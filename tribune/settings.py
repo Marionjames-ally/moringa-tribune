ae383aa8a20b1918d65caf3d891e20f994587009
Marionjames-ally <marionjames2018@gmail.com>
2020-02-27 09:45:41 +0300

    set debug to true

--- tribune/settings.py
+++ tribune/settings.py
@@ -27,7 +27,7 @@ MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 if config('MODE')=="dev":
    DATABASES = {
